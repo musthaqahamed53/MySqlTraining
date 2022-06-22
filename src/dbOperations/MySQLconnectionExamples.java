217aeac6c5ac136d0a1aa97f514fb5a41c937ab1
musthaqahamed53 <musthaq53ahamed@gmail.com>
2022-06-22 15:31:36 +0530

    update

--- src/dbOperations/MySQLconnectionExamples.java
+++ src/dbOperations/MySQLconnectionExamples.java
@@ -18,10 +18,14 @@ public class MySQLconnectionExamples {
 		//2.create connection withDB by giving the DB details
 		Connection connection = DriverManager.getConnection("jdbc:mysql://root@localhost/selenium_learning");
 		
+		//3.create a statement
 		Statement statement = connection.createStatement();
 		
+		//4.execute the query 
+		//5.store the result in result set
 		ResultSet result= statement.executeQuery("SELECT * FROM learn_table");
 		
+		//6. get the results from result set
 		while(result.next()) {
 			System.out.println("Name " +result.getString(1)+" Nickname " +result.getString(2));
 		}
